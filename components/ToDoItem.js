4f6f736793aef10f74bd44377aef470226797d03
Yurii <vasilvasilovichvasil@gmail.com>
2021-03-12 15:26:52 +0200

    delete icon added

--- components/ToDoItem.js
+++ components/ToDoItem.js
@@ -1,20 +1,31 @@
 import React from 'react'
-import { StyleSheet, Text, TouchableOpacity} from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View} from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
 
 const ToDoItem = ({item, deleteItem}) => {
     return (
-        <TouchableOpacity onPress={deleteItem}>
-            <Text style={styles.item}>{item.text}</Text>
+        <TouchableOpacity>
+            <View  style={styles.item}>
+                <MaterialIcons name={'delete-outline'} size={24} onPress={deleteItem}/>
+                <Text style={styles.text} >{item.text}</Text>
+            </View>
         </TouchableOpacity>
     );
 }
 const styles = StyleSheet.create({
     item: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
         padding: 16,
         marginTop: 10,
         borderWidth: 1,
         borderStyle: 'solid',
         borderRadius: 10
+    },
+    text: {
+        fontSize: 16,
+        marginHorizontal: 10
     }
 })
 
